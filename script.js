d1e162d6419e6bcf50270e09a5a3dd9ddb2d99ad
ArsenioDraco <voynovarseny@gmail.com>
2025-11-20 12:26:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function rnd(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function choice(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function round(num, decimals = 0) {
+  const factor = 10 ** decimals;
+  return Math.round(num * factor) / factor;
+}
+
+function simplifyFraction(n, d) {
+  function gcd(a,b){ return b===0 ? a : gcd(b, a%b); }
+  const g = gcd(n,d);
+  return [n/g, d/g];
+}
 
